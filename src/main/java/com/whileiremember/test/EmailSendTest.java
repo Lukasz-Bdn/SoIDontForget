235ad3709d1323df35603d0cf23972cf8b3767f5
Lukasz.Budyn <lukasz-budyn@wp.pl>
2018-02-12 13:40:57 +0100

    sendgrid setup and api_key env variable

--- src/main/java/com/whileiremember/test/EmailSendTest.java
+++ src/main/java/com/whileiremember/test/EmailSendTest.java
@@ -1,9 +1,31 @@
 package com.whileiremember.test;
 
+import java.io.IOException;
+
+import com.sendgrid.*;
 
 public class EmailSendTest {
+	
+	public static void main(String[] args) throws IOException {
+		Email from = new Email("test@example.com");
+		String subject = "no-reply Sending with SendGrid is Fun";
+		Email to = new Email("lukasz-budyn@wp.pl");
+		Content content = new Content("text/plain", "and easy to do anywhere, even with Java");
+		Mail mail = new Mail(from, subject, to, content);
+		
+		SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+		Request request = new Request();
 
-	public static void main(String[] args) {
+		try {
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			System.out.println(response.getStatusCode());
+			System.out.println(response.getBody());
+			System.out.println(response.getHeaders());
+		} catch (IOException ex) {
+			throw ex;
+		}
 	}
-
 }
---
+++ src/main/java/com/whileiremember/test/SendgridApiKeyTest.java
@@ -0,0 +1,8 @@
+package com.whileiremember.test;
+
+public class SendgridApiKeyTest {
+	public static void main(String[] args) {
+		String apiKey = System.getenv("SENDGRID_API_KEY");
+		System.out.println(apiKey);
+	}
+}
