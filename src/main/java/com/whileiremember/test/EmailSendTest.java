aeaeef14ccc204ce4a73ec2dd9e0342105856826
Lukasz.Budyn <lukasz-budyn@wp.pl>
2018-02-13 18:26:54 +0100

    EmailSenderTest changed to JUnit test

--- src/main/java/com/whileiremember/test/EmailSendTest.java
+++ src/main/java/com/whileiremember/test/EmailSendTest.java
@@ -1,32 +1,39 @@
 package com.whileiremember.test;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
+
+import org.junit.Test;
 
 import com.sendgrid.*;
 
 public class EmailSendTest {
-	
-//	public static void main(String[] args) throws IOException {
-//		Email from = new Email("test@lukaszbudyn.pl");
-//		String subject = "no-reply Sending with SendGrid is Fun (test whitelabel 6)";
-//		Email to = new Email("lukasz-budyn@wp.pl");
-//		Content content = new Content("text/plain", "and easy to do anywhere, even with Java "
-//				+ "whileiremember.lukaszbudyn.pl");
-//		Mail mail = new Mail(from, subject, to, content);
-//		
-//		SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-//		Request request = new Request();
-//
-//		try {
-//			request.setMethod(Method.POST);
-//			request.setEndpoint("mail/send");
-//			request.setBody(mail.build());
-//			Response response = sg.api(request);
-//			System.out.println(response.getStatusCode());
-//			System.out.println(response.getBody());
-//			System.out.println(response.getHeaders());
-//		} catch (IOException ex) {
+
+	@Test
+	public void test() {
+		Email from = new Email("test@lukaszbudyn.pl");
+		String subject = "no-reply Sending with SendGrid is Fun (test whitelabel 6)";
+		Email to = new Email("lukasz-budyn@wp.pl");
+		Content content = new Content("text/plain", "and easy to do anywhere, even with Java "
+				+ "whileiremember.lukaszbudyn.pl");
+		Mail mail = new Mail(from, subject, to, content);
+		
+		SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+		Request request = new Request();
+
+		try {
+			request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			System.out.println(response.getStatusCode());
+			System.out.println(response.getBody());
+			System.out.println(response.getHeaders());
+			assertTrue(response.getStatusCode()==202);
+		} catch (IOException ex) {
 //			throw ex;
-//		}
-//	}
+			fail();
+		}
+	}
 }
